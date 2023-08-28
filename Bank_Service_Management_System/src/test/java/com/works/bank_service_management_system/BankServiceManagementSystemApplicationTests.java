07ab5aaf033abe0856e8fd7dd03c7940a71fa246
Oğuzhan Kaymakçı <Oguzhankaymakci001@gmail.com>
2023-08-28 09:28:04 +0300

    db conf., app properties and packages

---
+++ Bank_Service_Management_System/src/test/java/com/works/bank_service_management_system/BankServiceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.works.bank_service_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServiceManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
